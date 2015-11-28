c37009c941df7433b0ad5a2b003fbf625b1202c9
quangta93 <quang.tran@scienergy.com>
2015-11-28 09:42:19 -0600

    Initial commit
    Setup meteor & react

---
+++ App.jsx
@@ -0,0 +1,8 @@
+
+App = React.createClass({
+
+  render() {
+    return <div>{'foo bar foo bar'}</div>
+  },
+
+});
---
+++ startup.jsx
@@ -0,0 +1,15 @@
+if (Meteor.isClient) {
+
+  // This code is executed on the client only
+
+  Meteor.startup(function () {
+    // Use Meteor.startup to render the component after the page is ready
+    React.render(<App />, document.getElementById("render-target"));
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
