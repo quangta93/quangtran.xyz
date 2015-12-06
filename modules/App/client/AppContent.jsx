7d4a4b9249706acfd1cc349c461c1f9647671874
quangta93 <quang.tran@scienergy.com>
2015-12-06 12:18:01 -0600

    Update project list

--- modules/App/client/AppContent.jsx
+++ modules/App/client/AppContent.jsx
@@ -2,3 +2,27 @@
 export const GREETING = "Hi, I am Quang Tran!";
 
 export const INTRODUCTION = "And this is the part where I am supposed to introduce myself to you.";
+
+export const PROJECTS = [
+  {
+    title: 'quangtran.xyz',
+    description: 'Personal website. Built with Meteor, ReactJS, and Stanley theme. Template from @thereactivestack/kickstart-simple.',
+    demoLink: 'http://quangtran.xyz/',
+    image: '/img/quangtran.xyz.png',
+    updated: 'Dec 2015',
+  },
+  {
+    title: 'CodeLab',
+    description: 'CodeLab is a realtime collaborative coding webapp with direct video calls, code sharing, rich messaging with automatic translation using the Kandy API, with the potential to be used for remote realtime code teaching and online interviews.',
+    demoLink: 'http://sonpham.me/r/codelab',
+    image: '/img/codelab.png',
+    updated: 'Feb 2015',
+  },
+  {
+    title: 'InstaJS',
+    description: 'A totally different JavaScript programming learning tool/webapp that tells exactly what variables change to what values and at what points (lines) in the program, including values of loop variables and function return, in realtime from top to bottom as the program runs; with Facebook Parse backend database integration for easy code snippets sharing.',
+    demoLink: 'http://sonpham.me/r/instajs',
+    image: '/img/instajs.png',
+    updated: 'Jan 2015',
+  },
+];
